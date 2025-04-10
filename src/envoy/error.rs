ec4777a6fd1d29e77cad8ff9434af60439ee8a20
gwm17 <gordonmccann215@gmail.com>
2025-04-10 13:50:57 -0400

    Add thiserror to remove a whole mess of error boilerplate

--- src/envoy/error.rs
+++ src/envoy/error.rs
@@ -2,193 +2,61 @@ use super::{
     ecc_operation::ECCOperation,
     message::{EmbassyMessage, MessageKind},
 };
+use thiserror::Error;
 use tokio::sync::{broadcast, mpsc};
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum ConversionError {
+    #[error("Could not convert string {0} to Operation/Status")]
     BadString(String),
 }
 
-impl std::fmt::Display for ConversionError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::BadString(s) => write!(f, "Could not convert string {s} to Operation/Status!"),
-        }
-    }
-}
-
-impl std::error::Error for ConversionError {}
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum EnvoyError {
-    BadRequest(reqwest::Error),
-    SendError(mpsc::error::SendError<EmbassyMessage>),
-    BadConversion(ConversionError),
-    FailedMessageParse(serde_json::Error),
-    InvalidStringToInt(std::num::ParseIntError),
-    InvalidStringToFloat(std::num::ParseFloatError),
-    FailedXMLParse(quick_xml::Error),
-    FailedXMLUtf8(std::string::FromUtf8Error),
+    #[error("Bad request: {0}")]
+    BadRequest(#[from] reqwest::Error),
+    #[error("Envoy failed to send a message: {0}")]
+    SendError(#[from] mpsc::error::SendError<EmbassyMessage>),
+    #[error("Message conversion failed: {0}")]
+    BadConversion(#[from] ConversionError),
+    #[error("Failed to parse message JSON: {0}")]
+    FailedMessageParse(#[from] serde_json::Error),
+    #[error("Failed to parse message integer: {0}")]
+    InvalidStringToInt(#[from] std::num::ParseIntError),
+    #[error("Failed to parse message float: {0}")]
+    InvalidStringToFloat(#[from] std::num::ParseFloatError),
+    #[error("Failed to parse message XML: {0}")]
+    FailedXMLParse(#[from] quick_xml::Error),
+    #[error("Failed to parse message XML as utf8: {0}")]
+    FailedXMLUtf8(#[from] std::string::FromUtf8Error),
+    #[error("Failed to parse message XML conversion")]
     FailedXMLConvert,
+    #[error("Sentry server had an internal error: {0}")]
     ServerError(String),
 }
 
-impl From<reqwest::Error> for EnvoyError {
-    fn from(value: reqwest::Error) -> Self {
-        Self::BadRequest(value)
-    }
-}
-
-impl From<mpsc::error::SendError<EmbassyMessage>> for EnvoyError {
-    fn from(value: mpsc::error::SendError<EmbassyMessage>) -> Self {
-        Self::SendError(value)
-    }
-}
-
-impl From<ConversionError> for EnvoyError {
-    fn from(value: ConversionError) -> Self {
-        Self::BadConversion(value)
-    }
-}
-
-impl From<serde_json::Error> for EnvoyError {
-    fn from(value: serde_json::Error) -> Self {
-        Self::FailedMessageParse(value)
-    }
-}
-
-impl From<std::num::ParseIntError> for EnvoyError {
-    fn from(value: std::num::ParseIntError) -> Self {
-        Self::InvalidStringToInt(value)
-    }
-}
-
-impl From<std::num::ParseFloatError> for EnvoyError {
-    fn from(value: std::num::ParseFloatError) -> Self {
-        Self::InvalidStringToFloat(value)
-    }
-}
-
-impl From<quick_xml::Error> for EnvoyError {
-    fn from(value: quick_xml::Error) -> Self {
-        Self::FailedXMLParse(value)
-    }
-}
-
-impl From<std::string::FromUtf8Error> for EnvoyError {
-    fn from(value: std::string::FromUtf8Error) -> Self {
-        Self::FailedXMLUtf8(value)
-    }
-}
-
-impl std::fmt::Display for EnvoyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::BadRequest(e) => {
-                write!(f, "Envoy recieved an error while making a request: {e}")
-            }
-            Self::FailedMessageParse(e) => {
-                write!(f, "Envoy failed to parse a message to yaml: {e}")
-            }
-            Self::BadConversion(e) => write!(f, "Envoy recieved conversion error: {e}"),
-            Self::SendError(e) => write!(f, "Envoy failed to send a message: {e}"),
-            Self::InvalidStringToInt(e) => {
-                write!(f, "Envoy failed to parse string to integer: {e}")
-            }
-            Self::InvalidStringToFloat(e) => {
-                write!(f, "Envoy failed to parse string to float: {e}")
-            }
-            Self::FailedXMLParse(e) => write!(f, "Envoy failed to parse XML body: {e}"),
-            Self::FailedXMLUtf8(e) => write!(f, "Envoy failed to convert XML to String: {e}"),
-            Self::FailedXMLConvert => write!(f, "Envoy failed to convert XML data!"),
-            Self::ServerError(e) => write!(f, "Server had an internal error: {e}"),
-        }
-    }
-}
-
-impl std::error::Error for EnvoyError {}
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum EmbassyError {
-    FailedMpscSend(mpsc::error::SendError<EmbassyMessage>),
-    FailedBroadcastSend(broadcast::error::SendError<EmbassyMessage>),
+    #[error("Embassy failed to send message: {0}")]
+    FailedMpscSend(#[from] mpsc::error::SendError<EmbassyMessage>),
+    #[error("Embassy failed to send cancel: {0}")]
+    FailedBroadcastSend(#[from] broadcast::error::SendError<EmbassyMessage>),
+    #[error("Embassy received invalid message kind: {0} expected: {1}")]
     InvalidKind(MessageKind, MessageKind),
-    FailedParse(serde_json::Error),
+    #[error("Embassy failed to parse message JSON: {0}")]
+    FailedParse(#[from] serde_json::Error),
+    #[error("Embassy failed to receive messages")]
     FailedRecieve,
-    FailedJoin(tokio::task::JoinError),
+    #[error("Embassy failed to join tasks: {0}")]
+    FailedJoin(#[from] tokio::task::JoinError),
+    #[error("Embassy received an invalid operation: {0}")]
     InvalidTransition(ECCOperation),
 }
 
-impl From<mpsc::error::SendError<EmbassyMessage>> for EmbassyError {
-    fn from(value: mpsc::error::SendError<EmbassyMessage>) -> Self {
-        Self::FailedMpscSend(value)
-    }
-}
-
-impl From<broadcast::error::SendError<EmbassyMessage>> for EmbassyError {
-    fn from(value: broadcast::error::SendError<EmbassyMessage>) -> Self {
-        Self::FailedBroadcastSend(value)
-    }
-}
-
-impl From<serde_json::Error> for EmbassyError {
-    fn from(value: serde_json::Error) -> Self {
-        Self::FailedParse(value)
-    }
-}
-
-impl From<tokio::task::JoinError> for EmbassyError {
-    fn from(value: tokio::task::JoinError) -> Self {
-        Self::FailedJoin(value)
-    }
-}
-
-impl std::fmt::Display for EmbassyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::InvalidKind(expected, recieved) => write!(
-                f,
-                "Embassy expected {expected} message, recieved {recieved} message!"
-            ),
-            Self::FailedMpscSend(e) => {
-                write!(f, "Embassy had an error sending the following message: {e}")
-            }
-            Self::FailedBroadcastSend(e) => {
-                write!(f, "Embassy had an error sending the following message: {e}")
-            }
-            Self::FailedParse(e) => write!(f, "Embassy had an error parsing a message: {e}"),
-            Self::FailedRecieve => {
-                write!(f, "Embassy communication lines were disconnected!")
-            }
-            Self::FailedJoin(e) => write!(f, "Embassy failed to join a task: {e}"),
-            Self::InvalidTransition(op) => write!(f, "Attempted invalid transition: {op}"),
-        }
-    }
-}
-
-impl std::error::Error for EmbassyError {}
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum BackupError {
-    FailedIO(std::io::Error),
+    #[error("Backup of config files failed due to: {0}")]
+    FailedIO(#[from] std::io::Error),
+    #[error("Config backup dir {0:?} already exists for run {1}")]
     AlreadyExists(std::path::PathBuf, i32),
 }
-
-impl From<std::io::Error> for BackupError {
-    fn from(value: std::io::Error) -> Self {
-        Self::FailedIO(value)
-    }
-}
-
-impl std::fmt::Display for BackupError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::FailedIO(e) => write!(f, "Backup of config files failed due to: {e}"),
-            Self::AlreadyExists(p, r) => {
-                write!(f, "Config backup dir {p:?} already exists for run {r}")
-            }
-        }
-    }
-}
-
-impl std::error::Error for BackupError {}
